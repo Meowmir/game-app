d9ea06a9798a0c442fe8b9aa8855d8dbe88adc81
Meowmir <disewji@gmail.com>
2023-06-12 11:24:32 +0200

    added find owner by ID, edit owner and delete owner to controller and service

--- src/cats-and-owners/owners/owners.controller.ts
+++ src/cats-and-owners/owners/owners.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { OwnersService } from './owners.service';
 import { CreateOwnerDto } from '../dto/create-owner.dto';
 
@@ -11,6 +22,15 @@ export class OwnersController {
     return this.ownersService.findAll();
   }
 
+  @Get('/:ownerID')
+  async getTodo(@Res() res: any, @Param('ownerID') ownerID: string) {
+    const owner = await this.ownersService.getOwner(ownerID);
+    if (!owner) {
+      throw new NotFoundException('Owner does not exist!');
+    }
+    return res.status(HttpStatus.OK).json(owner);
+  }
+
   // Create an owner
   @Post('/')
   async create(@Res() res: any, @Body() createOwnerDTO: CreateOwnerDto) {
@@ -18,6 +38,40 @@ export class OwnersController {
     return res.status(HttpStatus.OK).json({
       message: 'Owner has been submitted successfully!',
       owner: newOwner,
+    });
+  }
+
+  @Put('/:ownerID')
+  async editOwner(
+    @Res() res: any,
+    @Param('ownerID') ownerID: string,
+    @Body() createOwnerDTO: CreateOwnerDto,
+  ) {
+    const editedOwner = await this.ownersService.editOwner(
+      ownerID,
+      createOwnerDTO,
+    );
+    if (!editedOwner) {
+      throw new NotFoundException('Owner does not exist!');
+    }
+    return res.status(HttpStatus.OK).json({
+      message: 'Owner has been successfully updated',
+      owner: editedOwner,
+    });
+  }
+
+  @Delete('/:ownerID')
+  async deleteOwner(
+    @Res() res: any,
+    @Param('ownerID') ownerID: string,
+  ): Promise<string> {
+    const deletedOwner = await this.ownersService.deleteOwner(ownerID);
+    if (!deletedOwner) {
+      throw new NotFoundException('Owner does not exist!');
+    }
+    return res.status(HttpStatus.OK).json({
+      message: 'Owner has been deleted!',
+      owner: deletedOwner,
     });
   }
 }
--- src/cats-and-owners/owners/owners.service.ts
+++ src/cats-and-owners/owners/owners.service.ts
@@ -1,8 +1,14 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Owner } from '../schemas/owner.schema';
-import { CreateOwnerDto } from '../dto/create-owner.dto';
+import { CreateOwnerDto, updateOwnerDto } from '../dto/create-owner.dto';
+import { Cat } from '../schemas/cat.schema';
+import { updateCatDto } from '../dto/create-cat.dto';
 
 @Injectable()
 export class OwnersService {
@@ -17,5 +23,26 @@ export class OwnersService {
 
   async findAll(): Promise<Owner[]> {
     return this.ownerModel.find().exec();
+  }
+
+  async getOwner(ownerID: string): Promise<Owner> {
+    const foundOwner = await this.ownerModel.findById(ownerID);
+    if (!foundOwner) {
+      throw new BadRequestException(`Invalid ID ${ownerID}`);
+    }
+    return foundOwner;
+  }
+
+  async editOwner(ownerID: string, update: updateOwnerDto): Promise<Owner> {
+    await this.ownerModel.findByIdAndUpdate(ownerID, update);
+    return this.getOwner(ownerID);
+  }
+
+  async deleteOwner(ownerID: string): Promise<Owner> {
+    const foundOwner = await this.ownerModel.findByIdAndDelete(ownerID);
+    if (!foundOwner) {
+      throw new NotFoundException(`Invalid ID ${ownerID}`);
+    }
+    return foundOwner;
   }
 }

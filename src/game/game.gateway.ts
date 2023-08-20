00f20f4b81862219dc618cdd631154360e266255
Nora <106168179+Meowmir@users.noreply.github.com>
2023-08-20 21:08:14 +0200

    added watchers and listeners to dbservice, gameservice and gateway (#12)

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -9,17 +9,24 @@ import { Server } from 'socket.io';
 import { GameService } from './game.service';
 import { MessageDTO } from './DTO/messages.dto';
 import { ReadGameDTO } from './DTO/read-game.dto';
+import { OnModuleInit } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class GameGateway {
+export class GameGateway implements OnModuleInit {
   constructor(private gameService: GameService) {}
 
   @WebSocketServer()
   server: Server;
+
+  onModuleInit(): any {
+    this.gameService.addWatcher((game) => {
+      this.server.emit(`game/${game.gameId}`, game);
+    });
+  }
 
   @SubscribeMessage('game')
   //CHANGE DATA: ANY TO DATA: SCHEMA OR OTHER TYPE DESCRIPTION

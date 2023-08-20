a97e68e1143c8941261f5ad73fd472bc3269c1e6
Meowmir <disewji@gmail.com>
2023-08-20 21:07:38 +0200

    prod port

--- src/global.constants.ts
+++ src/global.constants.ts
@@ -1,2 +1,3 @@
 export const SERVER_FILE_ROOT = __dirname;
 export const API_TOKEN_HEADER = 'x-api-token';
+export const SERVER_PORT = process.env.NODE_ENV === 'production' ? 80 : 3000;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppGuard } from './app.guard';
+import { SERVER_PORT } from './global.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new AppGuard());
-  await app.listen(3000);
+  await app.listen(SERVER_PORT);
 }
 bootstrap();

207cdace50c7932554edc86385b25df22a165bfb
Nora <106168179+Meowmir@users.noreply.github.com>
2023-09-18 21:22:09 +0200

    Serve client (#23)

--- src/global.constants.ts
+++ src/global.constants.ts
@@ -1,5 +1,13 @@
+import { join } from 'path';
+import * as path from 'path';
+
 export const SERVER_FILE_ROOT = __dirname;
 export const API_TOKEN_HEADER = 'x-api-token';
 
 export const IS_PROD = process.env.NODE_ENV === 'production';
 export const SERVER_PORT = IS_PROD ? 80 : 3001;
+
+export const CLIENT_ROOT_DIR = path.join(
+  __dirname,
+  '../../color-fours-game/build',
+);

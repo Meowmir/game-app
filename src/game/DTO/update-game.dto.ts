cf9c09c701a860767b01776c8717892fea46ae2b
Meowmir <disewji@gmail.com>
2023-09-18 13:17:18 +0200

    changed filename

---
+++ src/game/DTO/update-game.dto.ts
@@ -0,0 +1,34 @@
+import { DTOBase } from './base.dto';
+import {
+  IsJSON,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { CreatePlayerDTO } from './create-player.dto';
+import { Player } from '../schemas/player-schema';
+
+export class UpdateGameDto extends DTOBase<UpdateGameDto> {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  state?: string;
+
+  @IsNumber()
+  @IsOptional()
+  turn?: number;
+
+  @IsJSON()
+  @IsOptional()
+  gameBoard?: string;
+
+  @ValidateNested()
+  @IsOptional()
+  players?: CreatePlayerDTO[];
+
+  @IsString()
+  @IsOptional()
+  winner?: Player;
+}
